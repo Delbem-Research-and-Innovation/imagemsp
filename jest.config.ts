86a1364c809de34dedf69080f4b7c98482ecbfcb
naneshoru <ricardo_cssg@hotmail.com>
2026-06-01 14:34:30 -0300

    chore: simplify jest config and update dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,4 @@ import { jestRootConfig } from '@ttoss/config';
 
 const config = jestRootConfig();
 
-export default {
-  ...config,
-  moduleNameMapper: {
-    ...config.moduleNameMapper,
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-};
+export default config;
